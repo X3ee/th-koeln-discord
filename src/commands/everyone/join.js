bafb2f9b9a8a88b000580ac51a1626f74f53fd9f
void <voidp@protonmail.com>
2020-01-04 02:47:47 +0100

    - fixed role channel check in join command - added join command functionality - added sqlite3 files to gitignore - minor changes to config - updated package-lock.json

--- src/commands/everyone/join.js
+++ src/commands/everyone/join.js
@@ -9,26 +9,37 @@ module.exports = class joinCommand extends commando.Command {
       'memberName': 'join',
       'aliases': ['add', 'beitreten'],
       'group': 'everyone',
-      'description': 'join a discord role',
+      'description': 'Füge eine Rolle hinzu',
       'guildOnly': true,
+      'argsPromptLimit': 0,
       'args': [
         {
           'key': 'role',
-          'prompt': 'which role do you want to join ?',
-          'type': 'role'
+          'prompt': 'Welche Rolle willst du hinzufügen ?',
+          'type': 'string'
         }
       ]
     });
   }
 
   run (msg, args) {
-    if (msg.channel.id === config.roleChannel) {
+    if (msg.channel.id !== config.roleChannel) {
       return;
     }
 
-    // TODO: check if user has already an role
-
     deleteCommandMessages(msg);
-    return msg.embed();
+
+    for (let key in config.roles) {
+      if (msg.member.roles.has(config.roles[key])) {
+        return msg.reply("Du hast bereits eine Rolle.");
+      }
+    };
+
+    if (config.roles.hasOwnProperty(args.role.toLowerCase())) {
+      msg.member.roles.add(config.roles[args.role.toLowerCase()]);
+      return msg.reply("Rolle hinzugefügt.");
+    } else {
+      return msg.reply("Diese Rolle existiert nicht.");
+    }
   }
 };
